387cec0a86ea9bf91341277275a903ea54d64299
Nadendla, Sai Sailesh (SHS TE DC IND DI-D&A DEV IIT-RAD CRB IMG) <sai-sailesh.nadendla@siemens-healthineers.com>
2025-09-15 20:47:41 +0530

    add sTest with basic functionality

--- Source/FileUploader.Deployment/Deploy/Scripts/start.ps1
+++ Source/FileUploader.Deployment/Deploy/Scripts/start.ps1
@@ -17,12 +17,16 @@ function Main {
     Start-Process powershell -ArgumentList "kubectl port-forward deployment/fileuploader-redis 6379:6379 -n file-uploader" -WindowStyle Hidden 
 
     $logFile = "..\..\Logs\FileUploader_API.log"
-    Start-Process -FilePath "..\..\APIServer\FileUploader.API.exe" -WindowStyle Hidden -RedirectStandardOutput $logFile  
+    #Start-Process -FilePath "..\..\APIServer\FileUploader.API.exe" -WindowStyle Hidden -RedirectStandardOutput $logFile  
+    Start-Process -FilePath (Join-Path $PSScriptRoot "..\..\APIServer\FileUploader.API.exe") -WorkingDirectory $PSScriptRoot -WindowStyle Hidden -RedirectStandardOutput $logFile
+
 
     $logFile = "..\..\Logs\FileUploader_Worker.log"
-    Start-Process -FilePath "..\..\Worker\FileUploader.Worker.exe" -WindowStyle Hidden -RedirectStandardOutput $logFile
+    #Start-Process -FilePath "..\..\Worker\FileUploader.Worker.exe" -WindowStyle Hidden -RedirectStandardOutput $logFile
+    Start-Process -FilePath (Join-Path $PSScriptRoot "..\..\Worker\FileUploader.Worker.exe") -WorkingDirectory $PSScriptRoot -WindowStyle Hidden -RedirectStandardOutput $logFile
 
-    Start-Process powershell -ArgumentList "dotnet ..\..\FileUploader.Client.dll"
+    $clientDLLPath = Join-Path $PSScriptRoot "..\..\FileUploader.Client.dll"
+    Start-Process powershell -ArgumentList "dotnet $clientDLLPath"
 
     Write-Host "All done!"
 }
---
+++ Test/FileUploader_sTest/FileUploaderSystemTests.cs
@@ -0,0 +1,67 @@
+﻿using FileUploader.Contracts;
+using NUnit.Framework;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace FileUploader_sTest
+{
+    public class FileUploaderSystemTests
+    {
+        private IConnectionMultiplexer _redis;
+        private const string _connectionString = "localhost:6379";
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            var configOptions = ConfigurationOptions.Parse(_connectionString);
+            configOptions.ConnectTimeout = 5000;
+            configOptions.SyncTimeout = 5000;
+            configOptions.AbortOnConnectFail = false;
+            configOptions.ConnectRetry = 3;
+            configOptions.ReconnectRetryPolicy = new ExponentialRetry(1000, 5000);
+
+            _redis =  await ConnectionMultiplexer.ConnectAsync(configOptions);
+        }
+
+        [Test]
+        public async Task FileUpload_ShouldTriggerUpdate()
+        {
+            var sub = _redis.GetSubscriber();
+            var receivedMessages = new List<string>();
+
+            await sub.SubscribeAsync("upload:updates", (_, value) =>
+            {
+                try
+                {
+                    var upd = JsonSerializer.Deserialize<UploadUpdate>(value);
+                    if (upd is null) return;
+
+                    receivedMessages.Add(value);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing update: {ex.Message}");
+                }
+            });
+
+            var pub = _redis.GetSubscriber();
+
+            var uploadJob = new UploadJobBuilder()
+                                .WithJobId(Guid.NewGuid())
+                                .WithFilePath(Path.GetTempPath())
+                                .WithFileName(Path.GetTempFileName())
+                                .WithFileSize("100")
+                                .WithAttempt(1)
+                                .Build();
+
+
+            var db = _redis.GetDatabase();
+            await db.ListLeftPushAsync("upload:jobs", JsonSerializer.Serialize(uploadJob))
+                .WaitAsync(TimeSpan.FromSeconds(5));
+
+            await Task.Delay(2000); // wait for async update
+
+            Assert.That(receivedMessages.Count(), Is.EqualTo(2));
+        }
+    }
+}
